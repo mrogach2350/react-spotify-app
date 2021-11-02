b056f199f213c03558d4f8b7b47c5f02be51136b
Michael Rogachevsky <m.rogach@gmail.com>
2021-11-01 22:43:02 -0700

    adds drag and drop to Playlist Details

--- src/components/PlaylistDetailCard.jsx
+++ src/components/PlaylistDetailCard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardHeader, CardContent } from '@mui/material';
 import { getPlaylistTracks } from '@/services/spotifyApi';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const PlaylistDetailCard = ({ selectedPlaylist }) => {
   const [tracks, setTracks] = useState([]);
@@ -13,14 +14,38 @@ const PlaylistDetailCard = ({ selectedPlaylist }) => {
     };
     fetchTracks();
   }, []);
+
+  const onDragEnd = () => {};
+
   return (
     <Card sx={{ margin: '25px' }}>
       <CardHeader title={selectedPlaylist.name} subheader={selectedPlaylist.description} />
-      <CardContent sx={{ maxHeight: '75vh', overflowY: 'scroll'  }}>
-        {tracks.map((track) => (
-          <div key={track.id}>{track.name}</div>
-        ))}
-      </CardContent>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId={selectedPlaylist.id}>
+          {(provided) => (
+            <CardContent
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              sx={{ maxHeight: '75vh', overflowY: 'scroll' }}
+            >
+              {tracks.map((track, index) => (
+                <Draggable key={track.id} draggableId={track.id} index={index}>
+                  {(provided) => (
+                    <div
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                    >
+                      {track.name}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </CardContent>
+          )}
+        </Droppable>
+      </DragDropContext>
     </Card>
   );
 };
